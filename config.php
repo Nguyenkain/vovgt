33282c225669dcbff137642cac7e0171499614b7
nguyenkain <nguyenkain@gmail.com>
2016-11-26 01:02:35 +0700

    First commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("MAP_API", 'AIzaSyBgqaIuAZeUgkbaM-fH3wCc3uYyx2U4oKg');
+define("DB_HOST", 'localhost');
+define("DB_NAME", 'vov');
+define("DB_USER", 'root');
+define("DB_PASS", '123123123');
